cd24eb2a0e56349ff4c19f486f42a4079f508e35
MichaelLuchin <m_luchin@mail.ru>
2025-04-14 23:51:12 +0400

    deleted first lab

--- main.cpp
+++ main.cpp
@@ -6,25 +6,6 @@
 
 using namespace std;
 
-int septToDec(string &sept){
-    int dec = 0;
-
-    for(int i = sept.size()-1; i >= 0; i--){
-        dec += (sept[i] - 48) * pow(7, sept.size()-i-1);
-    }
-    return dec;
-}
-
-bool isSept(string &sept){
-    for(char s : sept) {
-        if (s - 48 > 6) {
-            cout << "Not septenary or numeric";
-            return false;
-        }
-    }
-    return true;
-}
-
 int main(int argc, char* argv[]) {
     std::thread serverThread(server);
     client();
